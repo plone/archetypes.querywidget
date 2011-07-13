5e5ac75b5d5701335469bb9ca0b3b9fc233fa45d
Hanno Schlichting <hanno@hannosch.eu>
2011-07-13 09:48:32 +0000

    Metadata cleanup
    svn path=/MoreFieldsAndWidgets/archetypes.querywidget/trunk/; revision=13718

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup, find_packages
-import os
 
-version = '0.1'
-
-tests_require = ['plone.app.collection',
-                 'plone.app.testing']
+version = '1.0'
 
 setup(name='archetypes.querywidget',
       version=version,
       description="",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open("CHANGES.txt").read(),
       classifiers=[
         "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
-      author='Rob Gietema',
-      author_email='rob@fourdigits.nl',
-      url='http://plone.org',
-      license='GPL',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/archetypes.querywidget',
+      license='GPL version 2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['archetypes'],
       include_package_data=True,
@@ -29,9 +25,8 @@ setup(name='archetypes.querywidget',
           'setuptools',
           'plone.app.querystring',
       ],
-      tests_require=tests_require,
-      extras_require={'test': tests_require},
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
