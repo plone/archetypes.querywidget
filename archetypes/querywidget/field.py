9a90ebb357e939bb6a03fec132747e567951b888
Maurits van Rees <maurits@vanrees.org>
2013-03-28 23:50:31 +0100

    Build the query recursively.

--- archetypes/querywidget/field.py
+++ archetypes/querywidget/field.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from AccessControl import ClassSecurityInfo
+from Acquisition import aq_inner, aq_parent
 from archetypes.querywidget.interfaces import IQueryField
 from Products.Archetypes.Field import ObjectField
 from Products.Archetypes.Field import registerField
@@ -19,8 +20,10 @@ class QueryField(ObjectField):
     def get(self, instance, **kwargs):
         """Get the query dict from the request or from the object"""
         raw = kwargs.get('raw', None)
-        value = self.getRaw(instance)
-        if raw == True:
+        # By default we want to merge our query with our parent query.
+        recursive = kwargs.get('recursive', True)
+        value = self.getRaw(instance, recursive=recursive)
+        if raw:
             # We actually wanted the raw value, should have called getRaw
             return value
         querybuilder = QueryBuilder(instance, getSite().REQUEST)
@@ -34,7 +37,26 @@ class QueryField(ObjectField):
             limit=limit, brains=kwargs.get('brains', False))
 
     def getRaw(self, instance, **kwargs):
-        return deepcopy(ObjectField.get(self, instance, **kwargs) or [])
+        recursive = kwargs.get('recursive', False)
+        value = deepcopy(ObjectField.get(self, instance, **kwargs) or [])
+        if recursive:
+            parent = aq_parent(aq_inner(instance))
+            if self.getRaw(parent):
+                # The parent has the same field.  Combine it recursively.
+                parent_value = self.getRaw(parent, recursive=recursive)
+                for parent_row in parent_value:
+                    parent_index = parent_row['i']
+                    found = False
+                    for own_row in value:
+                        own_index = own_row['i']
+                        if own_index == parent_index:
+                            found = True
+                            break
+                    if not found:
+                        # parent index is not in own index, so we add it
+                        value.append(parent_row)
+
+        return value
 
 
 registerField(QueryField, title='QueryField',
