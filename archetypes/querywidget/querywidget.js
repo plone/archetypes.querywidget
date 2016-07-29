4f1af2948c85e76a883c1e24c53fa4d1961c60cb
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2016-07-29 16:00:40 -0300

    Fix base_url variable when editing a collection

--- archetypes/querywidget/querywidget.js
+++ archetypes/querywidget/querywidget.js
@@ -157,6 +157,7 @@
 
     $.querywidget.updateSearch = function () {
         var base_url = $("base").attr("href");
+        base_url = base_url.replace(/\/edit$/, '');
         if (base_url.indexOf("portal_factory") !== -1) {
             base_url = base_url.split("/").slice(0, -2).join("/");
         }
