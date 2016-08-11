02eb39c0ddd4a74ce71048979f1667f23428aed2
Maurits van Rees <maurits@vanrees.org>
2016-08-11 20:51:21 +0200

    Reindented setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -4,39 +4,41 @@ version = '1.1.3.dev0'
 
 tests_require = ['plone.app.testing']
 
-setup(name='archetypes.querywidget',
-      version=version,
-      description=("Archetypes.querywidget implements a widget "
-                   "for creating catalog queries using an "
-                   "email-filtering-like interface, as found "
-                   "in GMail or Apple Mail."),
-      long_description=open("README.rst").read() + "\n" + open(
-          "CHANGES.rst").read(),
-      classifiers=[
-          "Framework :: Plone",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python", ],
-      keywords='',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/archetypes.querywidget',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['archetypes'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.querystring>=1.2.2',  # custom_query support
-          'plone.app.jquery>=1.7.2',
-          'plone.app.jquerytools',
-      ],
-      tests_require=tests_require,
-      extras_require={
-          'tests': tests_require,
-      },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target=plone
-      """,
-      )
+setup(
+    name='archetypes.querywidget',
+    version=version,
+    description=("Archetypes.querywidget implements a widget "
+                 "for creating catalog queries using an "
+                 "email-filtering-like interface, as found "
+                 "in GMail or Apple Mail."),
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/archetypes.querywidget',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['archetypes'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.querystring>=1.2.2',  # custom_query support
+        'plone.app.jquery>=1.7.2',
+        'plone.app.jquerytools',
+    ],
+    tests_require=tests_require,
+    extras_require={
+        'tests': tests_require,
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target=plone
+    """,
+)
